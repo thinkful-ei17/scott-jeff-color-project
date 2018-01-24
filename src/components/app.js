d99403f62a29443cdd007c04dcdee6b4e6997d60
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-01-24 08:56:58 -0800

    stateful App with showItem

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,27 @@ import React from 'react';
 import Input from './input';
 import List from './list';
 
-export default function App(props){
-  return (
-    <section>
+export default class App extends React.Component {
+    
+  constructor(props){
+          super (props);
+          this.state = {
+            showList: false
+          };
+        }
+  render (){
+    if (this.state.showList){
+      return (
+        <section>
+          <Input />
+          <List />
+        </section>
+      );
+    }
+    return (
+     <section>
       <Input />
-      <List />
     </section>
-  )
+  );
+}
 }
